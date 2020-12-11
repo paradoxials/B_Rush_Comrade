f0676e3dd5da517d53d5b328620edf831c5da452
paradoxials <70249884+paradoxials@users.noreply.github.com>
2020-12-11 09:52:58 +0800

    Edited(NO ERRORS)

--- app/src/main/java/com/example/finalb_rushadmin/NavDashboard.java
+++ app/src/main/java/com/example/finalb_rushadmin/NavDashboard.java
@@ -7,19 +7,18 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class NavDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_nav_dashboard);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
