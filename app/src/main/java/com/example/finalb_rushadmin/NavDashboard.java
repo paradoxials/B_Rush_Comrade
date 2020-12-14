1b36c0d56648c7b055b06bf38c7cd137e435fec3
Rese574 <therese.bolabola@cit.edu>
2020-12-14 22:17:23 +0800

    does not exceed the max length

--- app/src/main/java/com/example/finalb_rushadmin/NavDashboard.java
+++ app/src/main/java/com/example/finalb_rushadmin/NavDashboard.java
@@ -7,11 +7,15 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -42,9 +46,6 @@ public class NavDashboard extends AppCompatActivity implements NavigationView.On
             navigationView.setCheckedItem(R.id.nav_dashboard);
         }
     }
-
-
-
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -81,6 +82,5 @@ public class NavDashboard extends AppCompatActivity implements NavigationView.On
         {
             super.onBackPressed();
         }
-
     }
 }
